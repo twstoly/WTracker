82f36265b44dc07abd27a9fdb5585f8a980891f5
twstoly <twstoly@gmail.com>
2022-02-06 10:57:44 +0300

    add method such as delete, save , getSightings to the Animal Class.

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -28,8 +28,67 @@ public class Animal extends Animals implements DatabaseManagement {
         }
     }
 
+    @Override
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name) VALUES (:name);";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
 
 
+    public static List<Animal> all() {
+        String sql = "SELECT * FROM animals;";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Animal.class);
+        }
+    }
 
+    public static Animal find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals WHERE id=:id;";
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetchFirst(Animal.class);
+        }
+    }
 
+    public void updateName(String name) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "UPDATE animals SET name=:name WHERE id=:id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .addParameter("name", name)
+                    .executeUpdate();
+        }
+    }
+
+    @Override
+    public void delete() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM animals WHERE id=:id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    public List<Sighting> getSightings() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM sightings WHERE animal_id=:id;";
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetch(Sighting.class);
+        }
+    }
 }
