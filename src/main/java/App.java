2254d54f86d77d0967d038612abe3c61812bd917
twstoly <twstoly@gmail.com>
2022-02-06 10:26:38 +0300

    add Main Class that creating a connection to heroku page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,140 @@
+import models.Animal;
+import models.Endangered;
+import models.Sighting;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+import static spark.Spark.get;
+
 public class App {
     public static void main(String[] args) {
 
+        ProcessBuilder process = new ProcessBuilder();
+        int port;
+
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
